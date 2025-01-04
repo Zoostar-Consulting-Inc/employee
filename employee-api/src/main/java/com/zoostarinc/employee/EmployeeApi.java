2c6ac9944189a4d3b9b221ea62d9bd895391b9f4
zoostar <devops@zoostar.net>
2025-01-03 23:39:11 -0500

    EMP-1: Initial maven checkin

--- employee-api/src/main/java/com/zoostarinc/employee/EmployeeApi.java
+++ employee-api/src/main/java/com/zoostarinc/employee/EmployeeApi.java
@@ -2,10 +2,10 @@ package com.zoostarinc.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import lombok.Generated;
 
@@ -13,15 +13,15 @@ import lombok.Generated;
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @ComponentScan(basePackages = { "net.zoostar", "com.zoostarinc" })
-public class EmployeeApi implements WebMvcConfigurer {
-
-	@Override
-	public void addViewControllers(final ViewControllerRegistry registry) {
-		registry.addRedirectViewController("/", "/swagger-ui/index.html");
-	}
+public class EmployeeApi extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeApi.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(EmployeeApi.class);
 	}
 
 }
--- employee-api/src/main/java/com/zoostarinc/employee/api/config/EmployeeApiConfig.java
+++ employee-api/src/main/java/com/zoostarinc/employee/api/config/EmployeeApiConfig.java
@@ -2,12 +2,19 @@ package com.zoostarinc.employee.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
-public class EmployeeApiConfig {
+public class EmployeeApiConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addViewControllers(final ViewControllerRegistry registry) {
+		registry.addRedirectViewController("/", "/swagger-ui/index.html");
+	}
 
 	@Bean
 	OpenAPI openAPI() {
