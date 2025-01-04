61f93a0b9952c860fa1abf340069fd6d1462c3a6
zoostar <devops@zoostar.net>
2025-01-04 00:32:17 -0500

    EMP-1: Initial maven checkin

--- employee-api/src/main/java/com/zoostarinc/employee/EmployeeApi.java
+++ employee-api/src/main/java/com/zoostarinc/employee/EmployeeApi.java
@@ -4,9 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import lombok.Generated;
 
 @Generated
@@ -22,6 +25,11 @@ public class EmployeeApi extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(EmployeeApi.class);
+	}
+
+	@Bean
+	OpenAPI openAPI() {
+		return new OpenAPI().info(new Info().title("Employee CRUD API"));
 	}
 
 }
--- employee-api/src/main/java/com/zoostarinc/employee/api/config/EmployeeApiConfig.java
+++ employee-api/src/main/java/com/zoostarinc/employee/api/config/EmployeeApiConfig.java
@@ -1,12 +1,8 @@
 package com.zoostarinc.employee.api.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
 public class EmployeeApiConfig implements WebMvcConfigurer {
@@ -14,11 +10,6 @@ public class EmployeeApiConfig implements WebMvcConfigurer {
 	@Override
 	public void addViewControllers(final ViewControllerRegistry registry) {
 		registry.addRedirectViewController("/", "/swagger-ui/index.html");
-	}
-
-	@Bean
-	OpenAPI openAPI() {
-		return new OpenAPI().info(new Info().title("Employee CRUD API"));
 	}
 
 }
