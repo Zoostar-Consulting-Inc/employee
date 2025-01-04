2dd9311ebb6c063e61752a6c35c78dc36cb821f4
zoostar <devops@zoostar.net>
2025-01-04 12:03:50 -0500

    EMP-1: Initial maven checkin

--- employee-api/src/main/java/com/zoostarinc/employee/api/config/EmployeeApiConfig.java
+++ employee-api/src/main/java/com/zoostarinc/employee/api/config/EmployeeApiConfig.java
@@ -1,11 +1,10 @@
 package com.zoostarinc.employee.api.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Profile("local")
+//@Profile("local")
 @Configuration
 public class EmployeeApiConfig implements WebMvcConfigurer {
 
