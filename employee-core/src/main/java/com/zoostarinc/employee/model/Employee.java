c45064d54d9611abb4bb561dbf5e63b11a687ba9
zoostar <devops@zoostar.net>
2025-06-07 22:07:50 -0400

    EMP-2: Update parent version.

--- employee-core/src/main/java/com/zoostarinc/employee/model/Employee.java
+++ employee-core/src/main/java/com/zoostarinc/employee/model/Employee.java
@@ -1,20 +1,39 @@
 package com.zoostarinc.employee.model;
 
+import java.time.LocalDate;
+import java.util.Collection;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import net.zoostar.common.core.Hierarchical;
 
 @Getter
 @Setter
 @ToString
-public class Employee {
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee implements Hierarchical<Employee> {
 
-	private String username;
-	
 	private String email;
 	
 	private String firstName;
 	
 	private String lastName;
 	
+	private LocalDate dateOfBirth;
+	
+	private Employee parent;
+	
+	private Collection<Employee> children;
+
+	@Override
+	public void addChild(Employee child) {
+		if(child != null) {
+			children.add(child);
+		}
+	}
+
 }
